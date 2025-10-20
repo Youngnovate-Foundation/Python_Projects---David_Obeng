4044a42bcd4e9f43ea18d87ca8e3a2b5dec1e191
Atiemo-Keseku David Obeng <datiemokeseku16@gmail.com>
2025-10-20 09:10:00 +0000

    Create user inputs

---
+++ password_generator.py
@@ -0,0 +1,16 @@
+import string
+
+"""
+    Password Generator
+    David Obeng Atiemo-Keseku
+
+"""
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+special_char = string.punctuation
+
+print('========================================')
+print("Welcom to the Password Generator App!")
+print('======================================== \n')
+
+length = int(input('Input length of passowrd: '))
