e2704bfc1b929eaa548a401950f3730cb21ea7cf
Atiemo-Keseku David Obeng <datiemokeseku16@gmail.com>
2025-10-20 09:48:02 +0000

    Complete password generator app

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,40 @@
-import string
-
 """
     Password Generator
     David Obeng Atiemo-Keseku
 
 """
+
+import string
+import secrets
+
 lower = string.ascii_lowercase
 upper = string.ascii_uppercase
 special_char = string.punctuation
 
+print('\n')
 print('========================================')
-print("Welcom to the Password Generator App!")
+print("Welcome to the Password Generator App!")
 print('======================================== \n')
 
-length = int(input('Input length of passowrd: '))
+while True:
+    length = int(input('Input length of Passowrd (Password must have at least 8 Characters): '))
+    print('\n')
+    if length >= 8:
+        break
+    else:
+        print('Input a number greater than 8. \n')
+
+while True:
+    choice = input('Include special characters in password (yes/no): ').lower()
+    print('\n')
+    if choice == 'yes':
+        char = lower + upper + special_char
+        break
+    elif choice == 'no':
+        char = lower + upper
+        break
+    else:
+        print("Input must be 'Yes' or 'No' for special characters inclusion. \n")
+
+password = ''.join(secrets.choice(char) for i in range(length))
+print(f"Generated Password {password}")
